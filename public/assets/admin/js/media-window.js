ac8e6bf67e88fae60fa0b83cee20223d7dc3837a
Efthakhar <efthakharbin@gmail.com>
2023-06-09 09:58:25 +0600

    Complete Media Window Functionality

--- public/assets/admin/js/media-window.js
+++ public/assets/admin/js/media-window.js
@@ -1,72 +1,87 @@
 $(function () {
 
     let select_multiple = false
+    let selected_media = { }
     let current_page = 1
     let last_page = 1
     let filename_serach = ''
 
-    let typingTimer;
+    let typingTimer
 
     $('.media-window-open-btn').on('click', (e) => {
 
         open_media_window(true)
+        .then((media)=>{
+            console.log(media)
+        })
     })
-
-
 
     function open_media_window(multiple) {
 
-        select_multiple = multiple
 
+        return new Promise((resolve,reject)=>{   
 
-        document.querySelector('.popups-wrapper').insertAdjacentHTML(
-            'beforeend',
-            `<div class="media-window-container">
-            <div class="media-window">
-                <div class="media-window-inner p-2">
-                    <div class="media-window-items row" id="media-window-items" >
-                        
-                    </div>
-                   
-                    <button class="btn btn-sm btn-info load-more-media-item mt-2 mb-5">
-                        <i class="ri-loader-2-line"></i> load more
-                    </button>
-                    <div class="mb-5"></div>
-                    <div class="media-window-actionbar border-top bg-white p-1 d-flex flex-wrap">
-                        <input type="text" class="" id='filename_search' placeholder="serach by name...."  >
-                        <div class="ms-auto" >
-                            <span class="btn btn-primary btn-sm">Insert Selected</span>
-                            <span class="btn btn-danger btn-sm ms-1 close-media-window">Close</span>
+            select_multiple = multiple
+            selected_media = { }
+            current_page = 1
+            last_page = 1
+            filename_serach = ''
+
+            document.querySelector('.popups-wrapper').insertAdjacentHTML(
+                'beforeend',
+                `<div class="media-window-container">
+                <div class="media-window">
+                    <div class="media-window-inner p-2">
+                        <div class="media-window-items row" id="media-window-items" >
+                            
+                        </div>
+                    
+                        <button class="btn btn-sm btn-info load-more-media-item mt-2 mb-5">
+                            <i class="ri-loader-2-line"></i> load more
+                        </button>
+                        <div class="mb-5"></div>
+                        <div class="media-window-actionbar border-top bg-white p-1 d-flex flex-wrap">
+                            <input type="text" class="" id='filename_search' placeholder="serach by name...."  >
+                            <div class="ms-auto" >
+                                <span class="btn btn-primary btn-sm insert-selected-media-btn">Insert Selected</span>
+                                <span class="btn btn-danger btn-sm ms-1 close-media-window">Close</span>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            </div>`
-        )
+                </div>`
+            )
 
-        $(document).on('click', '.load-more-media-item', function () {
-            current_page += 1
-            fetchMedia(current_page, filename_serach)
-        })
-
-        $(document).on('click', '.close-media-window', function () {
-            document.querySelector('.popups-wrapper').innerHTML = ``
-        })
-
-        let mediaWindowInner = document.querySelector('.media-window-inner')
-        mediaWindowInner.addEventListener('scroll', function () {
-
-            if (
-                (last_page != current_page) &&
-                (mediaWindowInner.scrollTop + mediaWindowInner.clientHeight >= mediaWindowInner.scrollHeight)
-            ) {
+            $(document).on('click', '.load-more-media-item', function () {
                 current_page += 1
                 fetchMedia(current_page, filename_serach)
-            }
+            })
 
-        })
+            $(document).on('click', '.close-media-window', function () {
+                document.querySelector('.popups-wrapper').innerHTML = ``
+                resolve({ })
+            })
 
-        fetchMedia(current_page, filename_serach)
+            $(document).on('click', '.insert-selected-media-btn', function () {
+                document.querySelector('.popups-wrapper').innerHTML = ``
+                resolve(selected_media)
+            })
+
+            let mediaWindowInner = document.querySelector('.media-window-inner')
+            mediaWindowInner.addEventListener('scroll', function () {
+
+                if (
+                    (last_page != current_page) &&
+                    (mediaWindowInner.scrollTop + mediaWindowInner.clientHeight >= mediaWindowInner.scrollHeight)
+                ) {
+                    current_page += 1
+                    fetchMedia(current_page, filename_serach)
+                }
+
+            })
+
+            fetchMedia(current_page, filename_serach)
+        })    
 
     }
 
@@ -79,13 +94,12 @@ $(function () {
             success: function (data) {
 
                 last_page = data.last_page
-                console.log(data.media)
                 let html = ``
                 data.media.forEach(item => {
                     html +=
                         `<div class="media-window-item p-1 col-md-2 col-sm-4 col-6">
                             <div class="card">
-                                <img class="card-img-top" src="${item.url}" alt="Card image cap">
+                                <img class="card-img-top" src="${item.url}" alt="Card image cap" data-media-id=${item.id}>
                                 <div class="card-body p-1 bg-light">
                                     <p class="card-text">${item.filename}.${item.extension} </p>
                                 </div>
@@ -109,15 +123,31 @@ $(function () {
         // select media item on click
         if (e.target.closest('.media-window-item')) {
 
-            if (select_multiple == false) {
+            let media_item = e.target.closest('.media-window-item')
 
+            let media_id = media_item.querySelector('img').getAttribute('data-media-id')
+            let media_src = media_item.querySelector('img').getAttribute('src')
+
+            if (select_multiple == false) {
+               
                 let media_items = document.querySelectorAll('.media-window-item')
                 for (let i = 0; i < media_items.length; i++) {
                     media_items[i].classList.remove('selected')
                 }
+                selected_media = { }
             }
 
-            e.target.closest('.media-window-item').classList.toggle('selected')
+        
+            if(media_item.classList.contains('selected')){
+                media_item.classList.remove('selected')
+                delete selected_media[media_id]
+            }else{
+                media_item.classList.add('selected')
+                selected_media[media_id] = { 'id'  : media_id, 'src' : media_src }
+            }
+
+
+            console.log(selected_media)
         }
 
     })
